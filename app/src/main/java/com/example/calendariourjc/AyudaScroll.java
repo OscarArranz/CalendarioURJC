f0789b539e8a68ba0138ade15366ade898e53d8d
Oscar Arranz <oscararranzp@gmail.com>
2021-11-21 13:06:56 +0100

    Fix Activity error, add to manifest

--- app/src/main/java/com/example/calendariourjc/AyudaScroll.java
+++ app/src/main/java/com/example/calendariourjc/AyudaScroll.java
@@ -1,9 +1,6 @@
 package com.example.calendariourjc;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,8 +10,8 @@ public class AyudaScroll extends AppCompatActivity {
 
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_ayudascroll);
 
