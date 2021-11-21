ef6e769bf342a7a928f794d2f961ac9c8f6cf5fa
rukusxd <fran.cardi.31@gmail.com>
2021-11-21 21:03:40 +0100

    ya sta 2

---
+++ app/src/main/java/com/example/calendariourjc/AyudaScroll.java
@@ -0,0 +1,25 @@
+package com.example.calendariourjc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AyudaScroll extends AppCompatActivity {
+
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        setContentView(R.layout.activity_ayudascroll);
+
+
+
+    }
+
+}
